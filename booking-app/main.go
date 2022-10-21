bd18b77cd55443bfdc47b887260cc616d4043bc1
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-10-21 14:38:12 +0200

    basic stdin, stdout, special vars with pointers

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = conferenceTickets
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T.\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend!")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	// read STDin with a pointer (&)
+	fmt.Print("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Print("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Print("Enter your email address: ")
+	fmt.Scan(&email)
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets! Your receipt will be sent to the email you provided: %v \n", firstName, lastName, userTickets, email)
 }
