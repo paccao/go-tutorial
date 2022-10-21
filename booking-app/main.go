abbe84bc960e5e601d9266c3107e5281d290875b
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-10-21 18:32:43 +0200

    arrays, slices, loops

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
-	const conferenceTickets int = 50
-	var remainingTickets int = conferenceTickets
+	const conferenceTickets uint = 50
+	var remainingTickets uint = conferenceTickets
+
+	// var bookings = [50]string{"Nana", "Peter"}
+	// var bookings [50]string{}
+	bookings := []string{}
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T.\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -13,19 +20,34 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend!")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
-	// read STDin with a pointer (&)
-	fmt.Print("Enter your first name: ")
-	fmt.Scan(&firstName)
-	fmt.Print("Enter your last name: ")
-	fmt.Scan(&lastName)
-	fmt.Print("Enter your email address: ")
-	fmt.Scan(&email)
-	fmt.Print("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Printf("Thank you %v %v for booking %v tickets! Your receipt will be sent to the email you provided: %v \n", firstName, lastName, userTickets, email)
+		// read STDin with a pointer (&)
+		fmt.Print("Enter your first name: ")
+		fmt.Scan(&firstName)
+		fmt.Print("Enter your last name: ")
+		fmt.Scan(&lastName)
+		fmt.Print("Enter your email address: ")
+		fmt.Scan(&email)
+		fmt.Print("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
+
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets! Your receipt will be sent to the email you provided: %v \n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+	}
 }
